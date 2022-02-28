205083ff37ab863e5974e067eec6d487f94d9f11
C-Noob404 <85572373+C-Noob404@users.noreply.github.com>
2022-02-28 17:25:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 echo "Install Luma.core drivers"
 apt update --fix-missing
 apt install python3-dev python3-pip libfreetype6-dev libjpeg-dev dsniff mitmproxy -y
-pip3 install --upgrade luma.oled
-pip3 install --upgrade luma.core
+pip install --upgrade luma.oled
+pip install --upgrade luma.core
 echo "Create directories"
 mkdir -p /root/BeBoXGui/{images,nmap}
 echo "Copying files"
